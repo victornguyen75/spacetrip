13b5dedfb15983e06f2ea97f12b18aa300a59d7d
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-09-19 16:18:00 -0500

    style(gumby.js): adjust gumby;

--- components/gumby.js
+++ components/gumby.js
@@ -4,11 +4,12 @@ const gumbyTexture = new THREE.TextureLoader().load("../img/gumby.jpg");
 const normalTexture = new THREE.TextureLoader().load("../img/normal.jpg");
 
 const gumby = new THREE.Mesh(
-  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.BoxGeometry(7, 7, 7),
   new THREE.MeshStandardMaterial({
     map: gumbyTexture,
     normalMap: normalTexture,
   })
 );
 
+gumby.position.set(0, 5, 0);
 export { gumby };
