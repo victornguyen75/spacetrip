6cc0d967d3fdaa050d260241af08c79015c1322a
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-09-19 13:23:32 -0500

    feat(index.html, main.js, styles.css): add dummy text for index.html; add movements for camera, moon, and gumby; add styles to complete the look;

--- main.js
+++ main.js
@@ -84,7 +84,26 @@ const moon = new THREE.Mesh(
   })
 );
 
+moon.position.z = 30;
+moon.position.setX(-10);
+
 scene.add(moon);
+
+const moveCamera = () => {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  gumby.rotation.y += 0.01;
+  gumby.rotation.z += 0.01;
+
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+  camera.position.z = t * -0.01;
+};
+
+document.body.onscroll = moveCamera;
 
 const animate = () => {
   requestAnimationFrame(animate);
