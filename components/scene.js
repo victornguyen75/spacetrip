ea08ba36892bfb740801fde200c200c041b3dc98
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-09-29 22:40:47 -0500

    Updates

--- components/scene.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-
-// A container for everything
-const scene = new THREE.Scene();
-const spaceTexture = new THREE.TextureLoader().load("../img/space.jpg");
-scene.background = spaceTexture;
-
-export { scene };
--- utils/utils.js
+++
@@ -1,6 +0,0 @@
-import * as THREE from "three";
-
-const lightHelper = (pointLight) => new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-
-export { lightHelper, gridHelper };
